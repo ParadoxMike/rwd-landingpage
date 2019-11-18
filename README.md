6ff32e6303942143b9ec082ccfa3757d649c9528
Mike <mike.blank@lantia.de>
2019-11-18 20:31:44 +0100

    setup twig stuff

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# rwd-landingpage
+# Responsive Webdesign Landingpage
+
+*Build modules as TWIG-Files inside the templates Folder!
+*You'll need a local PHP sever such as MAMP to develop with this Repro.
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+#ini_set('display_errors',1); # uncomment if you need debugging
+
+spl_autoload_register(function ($classname) {
+    $dirs = array (
+        './' #./path/to/dir_where_src_renamed_to_Twig_is_in
+    );
+
+    foreach ($dirs as $dir) {
+        $filename = $dir . str_replace('\\', '/', $classname) .'.php';
+        if (file_exists($filename)) {
+            require_once $filename;
+            break;
+        }
+    }
+
+});
+
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment($loader, [
+    'cache' => 'cache',
+]);
+
+echo $twig->render('base.twig', ['name' => 'Carlos']);
+
+?>
