eb407b15cce9229880791e8c7b234690accb4dff
Mike <mike.blank@lantia.de>
2020-01-11 01:08:18 +0100

    TASK: scroll library & color changeing

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,27 +3,66 @@
 var nSite = function ($) {
   $(function () {
     $.scrollify({
-      section: ".section"
+      section: ".section",
+      setHeights: false,
+      before: function before(index) {
+        changeActiveState(index);
+        changeColor(index);
+      }
     });
   });
   /******************************************************************
       VARS
   ******************************************************************/
-  // your code here
 
+  var $nSite = $('.n_site');
+  var $sections = $('.section');
+  var $logo = $('.o_logo__img');
+  var $navItems;
   /******************************************************************
       EVENTS
   ******************************************************************/
-  // your code here
 
+  buildNav();
+  $navItems.click(function () {
+    handleNavClick($(this));
+  });
   /******************************************************************
-      FUNCTIONS
-  ******************************************************************/
-  // your code here
+   FUNCTIONS
+   ******************************************************************/
 
+  function buildNav() {
+    $.each($sections, function (index, section) {
+      $nSite.append('<div class="n_site__nav-item" data-section-name="' + $(section).data("section-name") + '"><p class="n_site__nav-text">' + $(section).data("nav-name") + '</p></div>');
+    });
+    $navItems = $('.n_site__nav-item');
+  }
+
+  function changeActiveState(index) {
+    $(".n_site__nav-item--active").removeClass("n_site__nav-item--active");
+    $navItems.eq(index).addClass("n_site__nav-item--active");
+  }
+
+  function changeColor(index) {
+    if (index <= 1 || index >= 3) {
+      $nSite.removeClass("n_site--black");
+      $nSite.addClass("n_site--white");
+      $logo.attr("src", "./assets/img/logo/logo_white.svg");
+    } else {
+      $nSite.removeClass("n_site--white");
+      $nSite.addClass("n_site--black");
+      $logo.attr("src", "./assets/img/logo/logo_black.svg");
+    }
+  }
+
+  function handleNavClick($clickedNav) {
+    $.scrollify.move("#" + $clickedNav.data("section-name"));
+    console.log($clickedNav.data("section-name"));
+  }
   /******************************************************************
       PUBLIC_FUNCTIONS
   ******************************************************************/
+
 
   return {// your code here
   };
--- src/js/n_site.js
+++ src/js/n_site.js
@@ -2,31 +2,67 @@ const nSite = (function($) {
 
     $(function() {
         $.scrollify({
-          section : ".section",
+            section : ".section",
+            setHeights: false,
+            before:function(index) {
+                changeActiveState(index);
+                changeColor(index);
+            },
         });
-      });
+    });
 
 
     /******************************************************************
         VARS
     ******************************************************************/
-
-    // your code here
-
+    const $nSite = $('.n_site');
+    const $sections = $('.section');
+    const $logo = $('.o_logo__img');
+    let $navItems;
+    
 
     /******************************************************************
         EVENTS
     ******************************************************************/
+    buildNav();
 
-    // your code here
+    $navItems.click(function() {
+        handleNavClick($(this));
+    });
 
-
-    /******************************************************************
-        FUNCTIONS
+   /******************************************************************
+    FUNCTIONS
     ******************************************************************/
+   function buildNav() {
+        $.each( $sections, function(index, section){
+            $nSite.append('<div class="n_site__nav-item" data-section-name="' + $(section).data("section-name") + '"><p class="n_site__nav-text">' + $(section).data("nav-name") + '</p></div>');
+        });
+        $navItems = $('.n_site__nav-item');
+    }
 
-    // your code here
+    function changeActiveState(index) {
+        $(".n_site__nav-item--active").removeClass("n_site__nav-item--active");
+        $navItems.eq(index).addClass("n_site__nav-item--active");
+    }
 
+    function changeColor(index) {
+        if(index <= 1 || index >= 3) {
+            $nSite.removeClass("n_site--black");
+            $nSite.addClass("n_site--white");
+            $logo.attr("src", "./assets/img/logo/logo_white.svg");
+        }
+        else {
+            $nSite.removeClass("n_site--white");
+            $nSite.addClass("n_site--black");
+            $logo.attr("src", "./assets/img/logo/logo_black.svg");
+        }
+    }
+
+    function handleNavClick($clickedNav) {
+        $.scrollify.move("#" + $clickedNav.data("section-name"));
+        console.log($clickedNav.data("section-name"));
+        
+    }
 
     /******************************************************************
         PUBLIC_FUNCTIONS
